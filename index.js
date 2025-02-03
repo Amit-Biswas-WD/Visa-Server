b69f0e22c4f99a60a9f7d176aa5c14852018052c
Amit biswas <amitbiswas9282@gmail.com>
2025-02-03 23:48:11 +0600

    post

--- index.js
+++ index.js
@@ -1,19 +1,48 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
 const app = express();
-const port = process.env.POST || 5000;
+const port = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cors());
+
+const uri = `mongodb+srv://${process.env.USER_DB}:${process.env.PASS_DB}@cluster0.yit3t.mongodb.net/myDatabase?retryWrites=true&w=majority&appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+
+    const visaCollection = client.db("visaDB").collection("addVisa");
+
+    app.post("/addVisa", async (req, res) => {
+      const visa = req.body;
+      console.log("visa detains", visa);
+      const result = await visaCollection.insertOne(visa);
+      res.send(result);
+    });
+
+    console.log("Connected to MongoDB!");
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+  }
+}
+run();
 
 app.get("/", (req, res) => {
   res.send("Visa Server is Running.");
 });
 
 app.listen(port, () => {
-  console.log(`Visa Server port is ${port}`);
-});
-
-app.listen(port, () => {
-  console.log(`Visa Server port is ${port}`);
+  console.log(`Visa Server is running on port ${port}`);
 });
